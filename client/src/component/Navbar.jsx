464d342608de6d3550a7a87cb5a4a265e795497c
talbi nour <nt0506972@gmail.com>
2024-02-20 14:08:58 +0100

    nour

--- client/src/component/Navbar.jsx
+++ client/src/component/Navbar.jsx
@@ -1,27 +1,24 @@
-import React, { useState } from "react"
-import "./navbar.css"
-import { Link } from "react-router-dom"
-import { FaBars } from "react-icons/fa"
-import { ImCross } from "react-icons/im"
-import logoImage from '../assets/cevor-high-resolution-logo-transparent.png';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { FaBars } from "react-icons/fa";
+import { ImCross } from "react-icons/im";
+import logoImage from '../assets/cevor-high-resolution-logo-transparent (1).png';
+import Profile from "./Profile";
+import './navbar.css';
 
 
 const Navbar = () => {
-  const [Mobile, setMobile] = useState(false)
+  const [Mobile, setMobile] = useState(false);
+
   return (
     <>
       <nav className='navbar'>
-      <img src={logoImage} alt="Logo-png" className='logo-png' style={{ width: '50px', height: '50px' }} />
-      <h3 className='logo'>cevor</h3>
-      
+        <img src={logoImage} alt="Logo-png" className='logo-png' style={{  position: "sticky", left: '2',width: '5%', height: '50px' }} />
+        
 
-        {/*
-        if large screen ma xa bhane Mobile add huxa
-        if mobile screen ma xa bhane nav-links-mobile add huxa
-        */}
         <ul className={Mobile ? "nav-links-mobile" : "nav-links"} onClick={() => setMobile(false)}>
           <Link to='/' className='home'>
-            <li>Home</li>
+            <li>accuiel </li>
           </Link>
           <Link to='/about' className='about'>
             <li>About</li>
@@ -30,20 +27,19 @@ const Navbar = () => {
             <li>Services</li>
           </Link>
           <Link to='/skills' className='skills'>
-            <li>Skills</li>
+            <li>Test </li>
           </Link>
-          <Link to='/Login' className='Login'>
-            <li>Login</li>
+          <Link to='/login' className='login'>
+            <li>se_connecter</li>
           </Link>
+          <Profile /> {/* Ajouter le composant Profile */}
         </ul>
-        {/* 
-        whenever we click on button = setMobile(!Mobile) ==  is mobile oppsite to setMobile 
-        */}
         <button className='mobile-menu-icon' onClick={() => setMobile(!Mobile)}>
           {Mobile ? <ImCross /> : <FaBars />}
         </button>
       </nav>
     </>
-  )
+  );
 }
-export default Navbar
+
+export default Navbar;
