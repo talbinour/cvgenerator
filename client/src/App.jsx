69035a9c9770f3f05ebd6bd865bb0e05157fcf09
israa <isra.nasri@gmail.com>
2024-03-17 21:43:33 +0100

    israscc

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -22,6 +22,8 @@ import Model3 from "./Cv/model3";
 import Model4 from "./Cv/model4";
 import Model5 from "./Cv/model5";
 import Model6 from "./Cv/model6";
+import Model7 from "./Cv/model7";
+
 function App() {
   return (
     <>
@@ -47,6 +49,7 @@ function App() {
           <Route path='/Model4' element={<Model4 />} />
           <Route path='/Model5' element={<Model5 />} />
           <Route path='/Model6' element={<Model6 />} />
+          <Route path='/Model7' element={<Model7/>} />
           <Route path="/change-password/:email/:verificationCode" element={<NewPassword />} />
           {/* Add routes for Login and SignUp if needed */}
           {/* <Route path='/login' element={<Login />} exact />
