8126c244b26b594a4216cf4dd73b0ce2083dc02f
talbi nour <nt0506972@gmail.com>
2024-03-20 16:14:49 +0100

    css

--- client/src/component/Navbar.jsx
+++ client/src/component/Navbar.jsx
@@ -15,19 +15,7 @@ const Navbar = () => {
   
   useEffect(() => {
     const token = localStorage.getItem('token');
-  useEffect(() => {
-    const token = localStorage.getItem('token');
   
-    if (token) {
-      axios.get('http://localhost:8080/current-username', { withCredentials: true })
-        .then(response => {
-          setCurrentUser(response.data.user.nom);
-        })
-        .catch(error => {
-          console.error('Erreur lors de la récupération du nom d\'utilisateur :', error);
-        });
-    }
-  }, []);
     if (token) {
       axios.get('http://localhost:8080/current-username', { withCredentials: true })
         .then(response => {
