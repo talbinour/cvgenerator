20d7207378208aeafa41f923f8204a550df0ac52
israa <isra.nasri@gmail.com>
2024-04-27 15:59:04 +0100

    CVSCHAT

--- client/src/CvModelChat/CVModel7.jsx
+++ client/src/CvModelChat/CVModel7.jsx
@@ -1,11 +1,68 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Chat from "../chatbot";
-//import styles from '../Cv/model7.module.css'; // Assurez-vous d'avoir le fichier model5.module.css dans votre projet
+import axios from 'axios';
 import styles from './CVModel7.module.css'; // Assurez-vous d'avoir le fichier model5.module.css dans votre projet
 import '@fortawesome/fontawesome-free/css/all.css';
 import avatar from '../assets/cvprofile.jpeg';
 function CVModel7() {
   const [ setCvContent] = useState(""); // État pour stocker le contenu du CV
+  const [userId, setUserId] = useState(null);
+  const [cvModel, setCvModel] = useState({
+    name: 'John Doe',
+    jobTitle: 'Développeur Web',
+    phone: '0900 786 01',
+    email: 'emmi@gmail.com',
+    website: 'monsite.com',
+    linkedin: 'www.linkedin.com',
+    address: '56e rue, Californie',
+    education: [
+      { id: 1, period: '2017 - 2019', degree: 'Matric en Science', institution: 'Nom de l\'école' },
+      { id: 2, period: '2019 - 2021', degree: 'Intermédiaire en Maths', institution: 'Nom du collège' },
+      { id: 3, period: '2021 - Aujourd\'hui', degree: 'Licence en Informatique', institution: 'Nom de l\'université' }
+    ],
+    languages: [
+      { id: 1, name: 'Anglais', proficiency: 90 },
+      { id: 2, name: 'Ourdou', proficiency: 80 }
+    ],
+    profile: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Porro exercitationem nesciunt, tenetur architecto omnis maxime delectus quae quo reprehenderit quas laudantium. Itaque sequi commodi vero suscipit reiciendis ea aspernatur cum.',
+    experiences: [
+      { id: 1, period: '2019 - 2021', companyName: 'Société A', jobTitle: 'Développeur Web Senior', description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Porro exercitationem nesciunt, tenetur architecto omnis' },
+      { id: 2, period: '2021 - présent', companyName: 'Société B', jobTitle: 'Analyste de Données', description: 'Lorem ipsum,dolor sit amet consectetur adipisicing elit. Porro exercitationem nesciunt,tenetur architecto omnis' }
+    ],
+    professionalSkills: [
+      { id: 1, skillName: 'HTML', proficiency: 95 },
+      { id: 2, skillName: 'CSS', proficiency: 70 },
+      { id: 3, skillName: 'JavaScript', proficiency: 95 },
+      { id: 4, skillName: 'Python', proficiency: 75 }
+    ],
+    interests: ['Trading', 'Développement', 'Gaming', 'Business']
+  });
+
+  useEffect(() => {
+    const token = localStorage.getItem('token');
+
+    if (token) {
+      axios
+        .get('http://localhost:8080/current-username', { withCredentials: true })
+        .then((response) => {
+          const userData = response.data.user;
+          const userId = userData.id || userData.user_id;
+
+          setUserId(userId);
+          setCvModel({
+            ...cvModel,
+            name: userData.nom,
+            jobTitle: userData.prenom,
+            phone: userData.Nbphone,
+            email: userData.email,
+            address: userData.pays,
+          });
+        })
+        .catch((error) => {
+          console.error('Erreur lors de la récupération des informations utilisateur:', error);
+        });
+    }
+  }, [userId]);
 
   // Fonction pour mettre à jour le contenu du CV en fonction de la réponse de l'utilisateur
   const updateCvContent = (response) => {
@@ -24,30 +81,30 @@ function CVModel7() {
             <div className={styles.imgBx}>
               <img src={avatar} alt="Profile" />  
             </div>
-            <h2>Emaan Bari<br /><span>Computer Scientist</span></h2>
+            <h2>{cvModel.name} {cvModel.prenom}<br /><span>{cvModel.job}</span></h2>
           </div>
           <div className={styles.contactInfo}>
-            <h3 className={styles.title}>Contact Info</h3>
+            <h3 className={styles.title}>Informations de Contact</h3>
             <ul>
               <li>
                 <span className={styles.icon}><i className="fa fa-phone" aria-hidden="true"></i></span>
-                <span className={styles.text}>0900 786 01</span>
+                <span className={styles.text}>{cvModel.phone}</span>
               </li>
               <li>
                 <span className={styles.icon}><i className="fa fa-envelope" aria-hidden="true"></i></span>
-                <span className={styles.text}>emmi@gmail.com</span>
+                <span className={styles.text}>{cvModel.email}</span>
               </li>
               <li>
                 <span className={styles.icon}><i className="fa fa-globe" aria-hidden="true"></i></span>
-                <span className={styles.text}>mywebsite.com</span>
+                <span className={styles.text}>{cvModel.website}</span>
               </li>
               <li>
                 <span className={styles.icon}><i className="fa fa-linkedin" aria-hidden="true"></i></span>
-                <span className={styles.text}>www.linkedin.com</span>
+                <span className={styles.text}>{cvModel.linkedin}</span>
               </li>
               <li>
                 <span className={styles.icon}><i className="fa fa-map-marker" aria-hidden="true"></i></span>
-                <span className={styles.text}>56th street, california</span>
+                <span className={styles.text}>{cvModel.address}</span>
               </li>
             </ul>
           </div>
