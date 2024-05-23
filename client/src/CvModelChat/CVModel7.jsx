af7b2b08dc1331bccb9152f2abbbb636fe037205
israa <isra.nasri@gmail.com>
2024-05-23 15:36:54 +0100

    api

--- client/src/CvModelChat/CVModel7.jsx
+++ client/src/CvModelChat/CVModel7.jsx
@@ -1,16 +1,13 @@
 import React, { useState, useEffect } from "react";
 import Chat from "../chatbot/chatbot";
 import axios from "axios";
-//import PropTypes from 'prop-types'; // Ajout de l'importation de PropTypes
 import styles from "./CVModel7.module.css";
 import avatar from "../assets/cvprofile.jpeg";
 import { useNavigate } from "react-router-dom";
 
-
 const CVModel7 = () => {
   const navigate = useNavigate();
-  const [currentCVId,setCurrentCVId] = useState(null);
-
+  const [currentCVId, setCurrentCVId] = useState(null);
   const [userId, setUserId] = useState(null);
   const [userPhoto, setUserPhoto] = useState(null);
   const [cvModel, setCvModel] = useState({
@@ -29,8 +26,6 @@ const CVModel7 = () => {
     professionalSkills: [{ skillName: "", proficiency: "" }],
     interests: [],
   });
-  
-  
 
   useEffect(() => {
     const token = localStorage.getItem("token");
@@ -53,10 +48,10 @@ const CVModel7 = () => {
             website: userData.website,
             linkedin: userData.linkedin,
             address: userData.address,
-            education: [{ startDate: "", endDate: "", institution: "", degree: "" }],
+            education: [],
             languages: [],
             profile: "",
-            experiences: [{ startDate: "", endDate: "", ville: "", jobTitle: "", employeur: "", description: "" }],
+            experiences: [],
             professionalSkills: [],
             interests: [],
             photo: userData.photo,
@@ -71,7 +66,7 @@ const CVModel7 = () => {
   const updateUserResponse = (response, sectionKey, questionNumber) => {
     setCvModel((prevCvModel) => {
       const updatedModel = { ...prevCvModel };
-  
+
       switch (sectionKey) {
         case "contact info": {
           const contactInfoFields = ["phone", "email", "website", "linkedin", "address"];
@@ -81,34 +76,30 @@ const CVModel7 = () => {
         case "education": {
           const educationFields = ["startDate", "endDate", "institution", "degree"];
           const fieldIndex = (questionNumber - 1) % educationFields.length;
-  
+
           if (!Array.isArray(updatedModel.education)) {
             updatedModel.education = [];
           }
-  
-          // Si le champ actuel est "startDate", alors on commence une nouvelle entrée
+
           if (fieldIndex === 0) {
             updatedModel.education.push({});
           }
-  
-          // Met à jour la dernière entrée de la liste
+
           updatedModel.education[updatedModel.education.length - 1][educationFields[fieldIndex]] = response;
           break;
         }
         case "experience": {
           const experienceFields = ["startDate", "endDate", "ville", "jobTitle", "employeur", "description"];
           const fieldIndex = (questionNumber - 1) % experienceFields.length;
-  
+
           if (!Array.isArray(updatedModel.experiences)) {
             updatedModel.experiences = [];
           }
-  
-          // Si le champ actuel est "startDate", alors on commence une nouvelle entrée
+
           if (fieldIndex === 0) {
             updatedModel.experiences.push({});
           }
-  
-          // Met à jour la dernière entrée de la liste
+
           updatedModel.experiences[updatedModel.experiences.length - 1][experienceFields[fieldIndex]] = response;
           break;
         }
@@ -150,13 +141,10 @@ const CVModel7 = () => {
         default:
           break;
       }
-  
+
       return updatedModel;
     });
   };
-  
-  
-  
 
   const formatDate = (dateString) => {
     const date = new Date(dateString);
@@ -165,15 +153,14 @@ const CVModel7 = () => {
     const day = String(date.getDate()).padStart(2, "0");
     return `${year}-${month}-${day}`;
   };
+
   const validateAndFormatCVData = (cvModel) => {
     const formattedCVModel = { ...cvModel };
-  
-    // Assurez-vous que profile est une chaîne de caractères
+
     if (Array.isArray(formattedCVModel.profile)) {
       formattedCVModel.profile = formattedCVModel.profile.join(", ");
     }
-  
-    // Assurez-vous que toutes les dates sont présentes et au bon format
+
     formattedCVModel.education = formattedCVModel.education.map((edu) => {
       return {
         ...edu,
@@ -183,7 +170,7 @@ const CVModel7 = () => {
         }
       };
     });
-  
+
     formattedCVModel.experiences = formattedCVModel.experiences.map((exp) => {
       return {
         ...exp,
@@ -193,27 +180,26 @@ const CVModel7 = () => {
         }
       };
     });
-  
+
     return formattedCVModel;
   };
-  
+
   const saveCVToServer = async () => {
     try {
       const formattedCVModel = validateAndFormatCVData(cvModel);
-  
       const requiredFields = ['name', 'phone', 'email', 'address', 'profile'];
       const isEmptyField = requiredFields.some(field => !formattedCVModel[field]);
       if (isEmptyField) {
         alert('Veuillez remplir tous les champs obligatoires.');
         return;
       }
-  
+
       const cvId = currentCVId;
       if (!cvId) {
         console.error('CV ID is undefined');
         return;
       }
-  
+
       const response = await axios.post(`http://localhost:8080/cv/${userId}/${cvId}`, formattedCVModel);
       console.log('CV saved successfully:', response.data);
       const id = response.data.cvData._id;
@@ -222,6 +208,152 @@ const CVModel7 = () => {
       console.error('Error saving CV:', error);
     }
   };
+
+  const regenerateCVContent = async () => {
+    try {
+      const formattedCVModel = validateAndFormatCVData(cvModel);
+      const cvContentJSON = {
+        contents: [
+          {
+            parts: [
+              {
+                text: `Créer un CV professionnelle à partir des données suivantes et ,garder la meme  titre des section: ${JSON.stringify(formattedCVModel)}`
+              }
+            ]
+          }
+        ]
+      };
+
+      const apiKey = 'AIzaSyDUC4m-CZnwwcHCoFG_mUztkLCfEjskTmY';
+
+      const response = await axios.post(
+        `https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent?key=${apiKey}`,
+        cvContentJSON,
+        {
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        }
+      );
+
+      console.log('API response:', response.data);
+      
+      if (!response.data || !response.data.candidates || response.data.candidates.length === 0) {
+        console.error('Aucune donnée valide n\'a été renvoyée par l\'API.');
+        return;
+      }
+
+      const content = response.data.candidates[0]?.content?.parts[0]?.text;
+
+      if (!content) {
+        console.error('Le contenu retourné par l\'API est vide.');
+        return;
+      }
+
+      console.log('API response content:', content);
+
+      const newCVData = parseCVContent(content);
+
+      const updatedCvModel = {
+        ...cvModel,
+        name: newCVData.name,
+        prenom: newCVData.prenom,
+        profession: newCVData.profession,
+        phone: newCVData.phone,
+        email: newCVData.email,
+        website: newCVData.website,
+        linkedin: newCVData.linkedin,
+        address: newCVData.address,
+        profile: newCVData.profile,
+        education: newCVData.education || [],
+        languages: newCVData.languages || [],
+        experiences: newCVData.experiences || [],
+        professionalSkills: newCVData.professionalSkills || [],
+        interests: newCVData.interests || []
+      };
+
+      setCvModel(updatedCvModel);
+
+    } catch (error) {
+      console.error('Une erreur est survenue lors de la génération du contenu du CV :', error);
+    }
+  };
+
+  const parseCVContent = (content) => {
+    const lines = content.split('\n').filter(line => line.trim() !== '');
+    const newCVData = {
+      name: '',
+      prenom: '',
+      profession: '',
+      phone: '',
+      email: '',
+      website: '',
+      linkedin: '',
+      address: '',
+      profile: '',
+      education: [],
+      languages: [],
+      experiences: [],
+      professionalSkills: [],
+      interests: []
+    };
+  
+    let section = '';
+    lines.forEach(line => {
+      if (line.startsWith('**')) {
+        section = line.replace(/\*\*/g, '').toLowerCase();
+      } else {
+        switch (section) {
+          case 'Profil': {
+            newCVData.profile += line + ' ';
+            break;
+          }
+          case 'Compétences professionnelles': {
+            const [skill, proficiency] = line.split('(');
+            newCVData.professionalSkills.push({ skillName: skill.trim(), proficiency: parseInt(proficiency) });
+            break;
+          }
+          case 'Expériences': {
+            const [jobTitle, employer, period] = line.split(',');
+            const [startDate, endDate] = period.replace(')', '').split('(');
+            newCVData.experiences.push({
+              jobTitle: jobTitle.trim(),
+              employeur: employer.trim(),
+              startDate: new Date(startDate.trim()),
+              endDate: new Date(endDate.trim()),
+              ville: ''
+            });
+            break;
+          }
+          case 'Formation': {
+            const [degree, institution, period2] = line.split(',');
+            const [startDate2, endDate2] = period2.replace(')', '').split('(');
+            newCVData.education.push({
+              degree: degree.trim(),
+              institution: institution.trim(),
+              startDate: new Date(startDate2.trim()),
+              endDate: new Date(endDate2.trim())
+            });
+            break;
+          }
+          case 'langues': {
+            const [language, proficiency2] = line.split('(');
+            newCVData.languages.push({ name: language.trim(), proficiency: parseInt(proficiency2) });
+            break;
+          }
+          case '*Intérêts': {
+            newCVData.interests.push(line.trim());
+            break;
+          }
+          default:
+            break;
+        }
+      }
+    });
+  
+    return newCVData;
+  };
+  
   return (
     <div className={styles.pageWrapper}>
       <div className={styles.leftPanel}>
@@ -239,7 +371,7 @@ const CVModel7 = () => {
                 )}
               </div>
               <h2>{cvModel.name} <br />{cvModel.prenom}<br /></h2>
-            <h3>{cvModel.profession}</h3>       
+              <h3>{cvModel.profession}</h3>
             </div>
             <h3 className={styles.title}>CONTACT INFO</h3>
             <div className={styles.contactInfo}>
@@ -332,7 +464,7 @@ const CVModel7 = () => {
             <div className={styles.professional_skills}>
               <h2 className={styles.title2}>Compétences Professionnelles</h2>
               <div className={styles.skills}>
-              {cvModel.professionalSkills.map((skill, index) => (
+                {cvModel.professionalSkills.map((skill, index) => (
                   <div className={styles.skill} key={index}>
                     <span className={styles.skillName}>{skill.skillName}</span>
                     <div className={styles.progressBar}>
@@ -350,11 +482,10 @@ const CVModel7 = () => {
                 ))}
               </ul>
             </div>
-
-            
           </div>
         </div>
       </div>
+      <button onClick={regenerateCVContent} className={styles.regenerateButton}>Regenerate CV Content</button>
       <button onClick={saveCVToServer} className={styles.finishButton}>
         Sauvegarder le cv
       </button>
